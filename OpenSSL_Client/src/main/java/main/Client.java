4489cd61b192b4e00e340de5ea41f906a410dfad
sizaif <sizaif2000@gmail.com>
2022-08-24 00:14:20 +0800

    update two mode

--- OpenSSL_Client/src/main/java/main/Client.java
+++ OpenSSL_Client/src/main/java/main/Client.java
@@ -116,185 +116,194 @@ public class Client {
                 list.plotListing();
                 return;
             }
-            try {
 
-                while (!stop_soon) {
-                    stop_soon = true;
-
-                    config.getGeneralDelegate().setDebug(false);
-
-                    Config tlsConfig = config.createConfig();
-                    Client TlsClient = new Client();
-
-
-                    /**
-                     * 2022-08-15 21:35:22 TODO: 1. 在启动客户端之前，向fuzz传输信号，通知准备进行握手 2. 2022-08-15 目前暂时想到的 传输信号方式使用 socket
-                     * 本侧作为客户端socket_client，fuzz侧作为服务端socket_server 3. socket_client 写入当前时间unix 4. socket_server
-                     * 读取内容后，做hash 运算与pre_hash 进行比较如果不同，则认为启动了新客户端client与openssl s_server进行握手 此时fuzz 端将trace_ssL_bits
-                     * 清空， 并统计边覆盖率
-                     *
-                     */
-                    // 创建socket udp 客户端
-//                    DatagramSocket socket_client = new DatagramSocket(9997);
-//                    try {
-//
-//                        // 发送
-//                        long curTime = System.currentTimeMillis();
-//                        byte[] socket_data = long2byte(curTime);
-//                        System.out.println("socket_data: " + socket_data);
-//                        DatagramPacket socket_packet = new DatagramPacket(socket_data, socket_data.length,
-//                                InetAddress.getByName(config.getSocket_host()), config.getSocket_port());
-//                        // 发送数据包
-//                        socket_client.send(socket_packet);
-//                        System.out.println("send data success");
-//
-//                        // 接受
-//                        byte[] receive_buff = new byte[100];
-//                        boolean rec_ok = false;
-//                        while (!rec_ok) {
-//                            // 存放数据包的容器
-//                            DatagramPacket rec_packet = new DatagramPacket(receive_buff, 0, receive_buff.length);
-//                            socket_client.receive(rec_packet);
-//                            byte[] rec_packetData = rec_packet.getData();
-//                            String rec_data_str = new String(rec_packetData, StandardCharsets.UTF_8);
-//
-//                            System.out.println("rec_data: " + rec_data_str);
-//                            System.out.println("rec_data len : " + rec_data_str.length());
-//                            if (rec_data_str.startsWith("OK")) {
-//
-//                                System.out.println("receive ok!!!!!!!!!!");
-//                                rec_ok = true;
-//                            }
-//                        }
-//                    } catch (IOException ioex) {
-//                        System.out.println(ioex);
-//                    } finally {
-//                        if (socket_client != null) {
-//                            try {
-//                                socket_client.close();
-//                            } catch (Exception ioe) {
-//
-//                            }
-//                        }
-//                    }
-
-
-//                    WorkflowTrace trace = new WorkflowTrace();
-                    final int[] index = {0};
-                    if (config.getWorkflowsOutFiles() != null) {
-                        if (!Files.exists(Path.of(config.getWorkflowsOutFiles()))) {
-                            Files.createDirectory(Path.of(config.getWorkflowsOutFiles()));
-                        }
-                    }
-                    if (config.getWorkflowsInFiles() != null) {
-                        LOGGER.debug("Reading workflow trace files from " + config.getWorkflowsInFiles());
-                        Files.walkFileTree(Path.of(config.getWorkflowsInFiles()), new FileVisitor<Path>() {
-                            /**
-                             * 在访问任意目录前调用；
-                             *
-                             * @param dir
-                             * @param attrs
-                             * @return
-                             * @throws IOException
-                             */
-                            @Override
-                            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-
-                                LOGGER.debug("pre visit dir : " + dir);
-                                return FileVisitResult.CONTINUE;
-                            }
-
-                            /**
-                             * 访问到每个文件时调用；
-                             *
-                             * @param file
-                             * @param attrs
-                             * @return
-                             * @throws IOException
-                             */
-                            @Override
-                            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-//                                LOGGER.debug(" visit file is : "+ file);
-
-                                try {
-                                    index[0] += 1;
-
-                                    WorkflowTrace trace = WorkflowTraceSerializer.secureRead(new FileInputStream(file.toFile()));
-                                    State state = TlsClient.startTlsClient(tlsConfig, trace);
-
-                                    if (config.getWorkflowsOutFiles() != null) {
-
-
-                                        trace = state.getWorkflowTrace();
-                                        String outfilepre = config.getWorkflowsOutFiles();
-                                        String file_str = file.toString();
-                                        String temp_str[] = file_str.split("\\\\");
-                                        String old_file_name = temp_str[temp_str.length - 1];
-                                        String new_file_name = "Execute_" + old_file_name;
-                                        outfilepre = outfilepre + new_file_name;
-//                                        LOGGER.debug(" after file name : "+new_file_name +" ; after path: "+outfilepre);
-                                        LOGGER.debug("Writing workflow trace to " + outfilepre);
-                                        WorkflowTraceSerializer.write(new File(outfilepre), trace);
-                                    }
-
-                                } catch (Exception e) {
-//                                    e.printStackTrace();
-                                }
-
-                                return FileVisitResult.CONTINUE;
-                            }
-
-                            /**
-                             * 在访问文件失败是调用
-                             *
-                             * @param file
-                             * @param exc
-                             * @return
-                             * @throws IOException
-                             */
-                            @Override
-                            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-                                LOGGER.warn("visit failed : " + file);
-                                return FileVisitResult.CONTINUE;
-                            }
-
-                            /**
-                             * 在访问任意目录完成后调用；
-                             *
-                             * @param dir
-                             * @param exc
-                             * @return
-                             * @throws IOException
-                             */
-                            @Override
-                            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-                                return FileVisitResult.CONTINUE;
-                            }
-                        });
-
-
-                    }
-//                    if (config.getWorkflowInput() != null) {
-//                        LOGGER.debug("Reading workflow trace from " + config.getWorkflowInput());
-//                        trace = WorkflowTraceSerializer
-//                            .secureRead(new FileInputStream(new File(config.getWorkflowInput())));
-//                    }
-
-
-//                    if (config.getWorkflowOutput() != null) {
-//                        trace = state.getWorkflowTrace();
-//                        LOGGER.debug("Writing workflow trace to " + config.getWorkflowOutput());
-//                        WorkflowTraceSerializer.write(new File(config.getWorkflowOutput()), trace);
-//                    }
-
-                }
-            } catch (Exception e) {
-                LOGGER.error("Encountered an uncaught Exception aborting. See debug for more info.", e);
-            }
+            startWithNoUdp(config);
         } catch (ParameterException e) {
             LOGGER.error("Could not parse provided parameters. " + e.getLocalizedMessage(), e);
             commander.usage();
         }
+
+    }
+
+    public static void startWithUdp(ClientCommandConfig config) {
+        try {
+            while (!stop_soon) {
+                stop_soon = true;
+                config.getGeneralDelegate().setDebug(false);
+                Config tlsConfig = config.createConfig();
+                Client TlsClient = new Client();
+
+
+                /**
+                 * 2022-08-15 21:35:22
+                 * 1. 在启动客户端之前，向fuzz传输信号，通知准备进行握手
+                 * 2. 2022-08-15 目前暂时想到的 传输信号方式使用 socket 本侧作为客户端socket_client，fuzz侧作为服务端socket_server
+                 * 3. socket_client 写入当前时间unix
+                 * 4. socket_server 读取内容后，做hash 运算与pre_hash 进行比较如果不同，
+                 * 则认为启动了新客户端client与openssl s_server进行握手 此时fuzz 端将trace_ssL_bits清空， 并统计边覆盖率
+                 *
+                 */
+                // 创建socket udp 客户端
+                DatagramSocket socket_client = new DatagramSocket(9997);
+                try {
+
+                    // 发送
+                    long curTime = System.currentTimeMillis();
+                    byte[] socket_data = long2byte(curTime);
+                    System.out.println("socket_data: " + socket_data);
+                    DatagramPacket socket_packet = new DatagramPacket(socket_data, socket_data.length,
+                            InetAddress.getByName(config.getSocket_host()), config.getSocket_port());
+                    // 发送数据包
+                    socket_client.send(socket_packet);
+                    System.out.println("send data success");
+
+                    // 接受
+                    byte[] receive_buff = new byte[100];
+                    boolean rec_ok = false;
+                    while (!rec_ok) {
+                        // 存放数据包的容器
+                        DatagramPacket rec_packet = new DatagramPacket(receive_buff, 0, receive_buff.length);
+                        socket_client.receive(rec_packet);
+                        byte[] rec_packetData = rec_packet.getData();
+                        String rec_data_str = new String(rec_packetData, StandardCharsets.UTF_8);
+
+                        System.out.println("rec_data: " + rec_data_str);
+                        System.out.println("rec_data len : " + rec_data_str.length());
+                        if (rec_data_str.startsWith("OK")) {
+
+                            System.out.println("receive ok!!!!!!!!!!");
+                            rec_ok = true;
+                        }
+                    }
+                } catch (IOException ioex) {
+                    System.out.println(ioex);
+                } finally {
+                    if (socket_client != null) {
+                        try {
+                            socket_client.close();
+                        } catch (Exception ioe) {
+
+                        }
+                    }
+                }
+                startWorkflow(config, TlsClient);
+            }
+        } catch (Exception e) {
+            LOGGER.error("Encountered an uncaught Exception aborting. See debug for more info.", e);
+        }
+    }
+
+    public static void startWithNoUdp(ClientCommandConfig config) {
+        try {
+            while (!stop_soon) {
+                stop_soon = true;
+
+                config.getGeneralDelegate().setDebug(false);
+
+                Client TlsClient = new Client();
+
+                startWorkflow(config, TlsClient);
+            }
+
+        } catch (Exception e) {
+            LOGGER.error("Encountered an uncaught Exception aborting. See debug for more info.", e);
+        }
+    }
+
+    public static void startWorkflow(ClientCommandConfig config, Client TlsClient) throws IOException {
+
+        Config tlsConfig = config.createConfig();
+
+        if (config.getWorkflowsOutFiles() != null) {
+            if (!Files.exists(Path.of(config.getWorkflowsOutFiles()))) {
+                Files.createDirectory(Path.of(config.getWorkflowsOutFiles()));
+            }
+        }
+        if (config.getWorkflowsInFiles() != null) {
+            LOGGER.debug("Reading workflow trace files from " + config.getWorkflowsInFiles());
+            Files.walkFileTree(Path.of(config.getWorkflowsInFiles()), new FileVisitor<Path>() {
+                /**
+                 * 在访问任意目录前调用；
+                 *
+                 * @param dir
+                 * @param attrs
+                 * @throws IOException
+                 */
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+
+                    LOGGER.debug("pre visit dir : " + dir);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                /**
+                 * 访问到每个文件时调用；
+                 *
+                 * @param file
+                 * @param attrs
+                 * @throws IOException
+                 */
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+//                                LOGGER.debug(" visit file is : "+ file);
+
+                    try {
+                        WorkflowTrace trace = WorkflowTraceSerializer.secureRead(new FileInputStream(file.toFile()));
+                        State state = TlsClient.startTlsClient(tlsConfig, trace);
+
+                        if (config.getWorkflowsOutFiles() != null) {
+
+
+                            trace = state.getWorkflowTrace();
+
+                            String outfilepre = config.getWorkflowsOutFiles();
+                            String file_str = file.toString();
+                            String temp_str[] = file_str.split("\\\\");
+                            String old_file_name = temp_str[temp_str.length - 1];
+                            String new_file_name = "Execute_" + old_file_name;
+                            outfilepre = outfilepre + new_file_name;
+//                                        LOGGER.debug(" after file name : "+new_file_name +" ; after path: "+outfilepre);
+                            LOGGER.debug("Writing workflow trace to " + outfilepre);
+                            WorkflowTraceSerializer.write(new File(outfilepre), trace);
+                        }
+
+                    } catch (Exception e) {
+//                                    e.printStackTrace();
+                    }
+
+                    return FileVisitResult.CONTINUE;
+                }
+
+                /**
+                 * 在访问文件失败是调用
+                 *
+                 * @param file
+                 * @param exc
+                 * @throws IOException
+                 */
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    LOGGER.warn("visit failed : " + file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                /**
+                 * 在访问任意目录完成后调用；
+                 *
+                 * @param dir
+                 * @param exc
+                 * @return
+                 * @throws IOException
+                 */
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+
+        }
+
 
     }
 
